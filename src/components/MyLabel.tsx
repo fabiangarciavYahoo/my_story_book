aeb20bc0b5d75c1903f47c862a9b13ab0ecab7a8
fabiangarciaYahoo <fabiangarciav@yahoo.com>
2025-07-31 22:50:32 -0600

    initial commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,26 @@
+import './myLabel.css'
+
+interface Props {
+  label: string;
+  color: 'text-primary' | 'text-secondary' | 'text-tertiary';
+  allCaps: boolean;
+  size?: 'normal' | 'h1' | 'h2' | 'h3';
+  backgroundColor?: string;
+}
+
+export const MyLabel = ({
+  label,
+  color,
+  allCaps,
+  size = 'normal',
+  backgroundColor = 'transparent'
+}: Props) => {
+  return (
+    <span style={
+      {
+        textTransform: allCaps ? 'uppercase' : 'none',
+        backgroundColor
+      }
+    } className={`${size} ${color}`}>{label}</span>
+  )
+}
---
+++ src/stories/assets/MyLabel.stories.ts
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { MyLabel } from "../../components/MyLabel";
+
+const meta = {
+  title: 'MyLabel',
+  component: MyLabel,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {
+    size: {
+      control: 'inline-radio'
+    },
+    color: {
+      control: 'select'
+    }
+  }
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {
+  args: {
+    label: 'Basic',
+    color: 'text-primary',
+    allCaps: false,
+    size: 'normal',
+    backgroundColor: 'blue'
+  }
+};
+
+export const AllCaps: Story = {
+  args: {
+    label: 'All Caps',
+    color: 'text-tertiary',
+    allCaps: true,
+    size: 'normal',
+    backgroundColor: 'magenta'
+  }
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Secondary',
+    color: 'text-secondary',
+    allCaps: false,
+    size: 'normal',
+    backgroundColor: 'red'
+    }
+};
+
+export const CustomColor: Story = {
+  args: {
+    label: 'Custom Color',
+    color: 'text-primary',
+    allCaps: false,
+    size: 'normal',
+    backgroundColor: 'cyan'
+    }
+};
